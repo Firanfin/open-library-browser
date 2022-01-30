0667fb2a494be763c793e32e32a97aa2ee9c1f9e
Rami-Roope Uusitalo <firanfin@gmail.com>
2022-01-30 19:40:14 +0200

    Enhance Search component
    Rename SearchBox to Search. Enable searching by pressing
    return.

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,21 +1,26 @@
 import React, { FormEvent } from "react";
-import styles from "./SearchBox.module.css";
+import styles from "./Search.module.css";
 
-interface SearchBoxProps {
+interface SearchProps {
   onSearch: () => void;
   onTextChange: (value: string) => void;
   error: string;
 }
 
-function SearchBox({ onSearch, onTextChange, error }: SearchBoxProps) {
+function Search({ onSearch, onTextChange, error }: SearchProps) {
+
+  function handleSubmit(e: FormEvent) {
+    onSearch();
+    e.preventDefault();
+  }
 
   return (
-    <div className={styles.container}>
+    <form className={styles.container} onSubmit={handleSubmit}>
       <input type="text" className={styles.input} onChange={(e) => onTextChange(e.target.value)} />
       <button className={styles.button} onClick={onSearch}>Search</button>
       <div className={styles.error}>{error}</div>
-    </div>
+    </form>
   );
 }
 
-export default SearchBox;
+export default Search;
